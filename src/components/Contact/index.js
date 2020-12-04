23c2f64f7bcdc81b150377a402b7bad6e99e854e
Solomon <solomonmeresa8@gmail.com>
2020-12-03 17:29:30 -0800

    adds form validation

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,17 +1,39 @@
 import React ,{useState} from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 
 function ContactForm(){
+   const [errorMessage, setErrorMessage] = useState('');
 
    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
    const { name, email, message } = formState;
    
    function handleChange(e) {
-      setFormState({...formState, [e.target.name]: e.target.value })
+
+       if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            // isValid conditional statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+
+       if (!errorMessage) {
+          setFormState({ ...formState, [e.target.name]: e.target.value });
+        }
    }
 
    function handleSubmit(e) {
-       
+
         e.preventDefault();
         console.log(formState);
    }
