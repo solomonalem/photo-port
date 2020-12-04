30b90b9a65f7ce22845e0a69df3b8af2ecb36f1a
Solomon <solomonmeresa8@gmail.com>
2020-12-03 18:41:11 -0800

    adds test case for contact component

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -3,22 +3,28 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
+afterEach(cleanup);
+
 const categories = [
   { name: 'portraits', description: 'Portraits of people in my life' }
 ]
 const mockCurrentCategory = jest.fn();
 const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
-afterEach(cleanup);
+
 
 describe('Nav component',()=>{
     // baseline test
-    it('renders',( )=> {
-         render(<Nav
+   it('renders', () => {
+        render(<Nav
             categories={categories}
             setCurrentCategory={mockSetCurrentCategory}
             currentCategory={mockCurrentCategory}
-        />);
+            contactSelected={mockContactSelected}
+            setContactSelected={mockSetContactSelected}
+    />);
     })
 
 
